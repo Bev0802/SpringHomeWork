61ab9c3478445ec6b5a80d109859c7f8d384dee5
Katy <bev08021982@gmail.com>
2024-02-14 20:48:23 +0400

    HW_7

---
+++ hw_7/security_web_boot/src/main/java/com/geekbrains/bev0802/security_web_boot/PrivateDataController.java
@@ -0,0 +1,2 @@
+package com.geekbrains.bev0802.security_web_boot;public class PrivateDataController {
+}
---
+++ hw_7/security_web_boot/src/main/java/com/geekbrains/bev0802/security_web_boot/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.geekbrains.bev0802.security_web_boot;
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigurerAdapter {
+}
